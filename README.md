227a36dcfe222f63a6563eca7effbbbc4eee7614
lesimon <lesimon@student.ethz.ch>
2020-05-17 13:43:10 +0200

    Upload current progress

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mdatom
+
+The syntax for calculating the correlation is: ./mdatom inputFiles/params.inp inputFiles/velocities.traj
--- mdatom/source_code/lib/CorrelationCalculator.cpp
+++ mdatom/source_code/lib/CorrelationCalculator.cpp
@@ -1,5 +1,7 @@
 #include "CorrelationCalculator.h"
 
+#include <fftw3.h>
+
 #include <iomanip>
 #include <iostream>
 
@@ -19,18 +21,47 @@ void CorrelationCalculator::printCorrelation()
 
 void CorrelationCalculator::calculateCorrelation()
 {
-    if (fReader->getCalculationType() == 0) {
+    if (fReader->getCalculationType() == 0) { // direct multiplication method
         for (std::size_t i = 0; i < nTimeframes; ++i) {
             for (std::size_t j = 0; j < nAtoms; ++j) {
                 double sum{0};
                 for (std::size_t k = 0; k < nTimeframes-i; ++k) {
-                    sum += velocities.at(i).at(k)*velocities.at(i).at(k+i);
+                    sum += velocities.at(i).at(j)*velocities.at(k+i).at(j);
                 }
-                correlations[i] += sum/(nTimeframes-i);
+                correlations[i] += (sum/(nTimeframes-i));
             }
             correlations[i] /= nAtoms;
         }
+    } else if (fReader->getCalculationType() == 1) { // FFT method
+        for (std::size_t i = 0; i < nAtoms; ++i) {
+            fftw_complex *in, *out;
+            int N = static_cast<int>(nTimeframes)*2; // due to zero filling.
+            in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+            out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+            for (int j = 0; j < nTimeframes; ++j) {
+                in[j][0] = velocities.at(j).at(i);
+                in[j+nTimeframes][0] = in[j][1] = in[j+nTimeframes][1] = 0;
+            }
+            fftw_plan p = fftw_plan_dft_1d(N, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);
+
+            fftw_execute(p);
+            for (int j = 0; j < 2*nTimeframes; ++j) {
+                // multiplication with complex conjugate
+                in[j][0] = out[j][0]*out[j][0] + out[j][1]*out[j][1];
+                in[j][1] = out[j][0] = out[j][1] = 0;
+            }
+            // contrary to what the name suggests, this is a back transform.
+            fftw_plan p2 = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+            fftw_execute(p2);
+            
+            for (int j = 0; j < nTimeframes; ++j) {
+                correlations[j] += (out[j][0]/(2*nTimeframes*(nTimeframes-j)));
+            }
+        }
+        for (int j = 0; j < nTimeframes; ++j) {
+            correlations[j] /= nAtoms;
+        }
     } else {
-        std::cout << "Unimplemented for the time being.\n";
+        std::cout << "Please set XVOutput to 0 (direct multiplication method) or 1 (FFT method)\n";
     }
 }
